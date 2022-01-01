3b75293f964df75f488801d5dc7a5644ea1d2f08
alawa2020 <huder13168@gmail.com>
2021-12-31 20:41:40 -0500

    🏗️ BudgetingContext added

--- src/BudgetingApp.js
+++ src/BudgetingApp.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import BudgetingContext from './context/BudgetingContext';
 import AppRouter from './routers/AppRouter';
 
 const BudgetingApp = () => {
   return (
     <div>
-      <AppRouter />
+      <BudgetingContext.Provider value={{ name: 'james' }}>
+        <AppRouter />
+      </BudgetingContext.Provider>
     </div>
   );
 };
---
+++ src/context/BudgetingContext.js
@@ -0,0 +1,5 @@
+const { createContext } = require('react');
+
+const BudgetingContext = createContext(null);
+
+export default BudgetingContext;
