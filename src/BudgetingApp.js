70f1df2e0177b38beca53b71839f70c663b8506a
alawa2020 <huder13168@gmail.com>
2021-12-31 20:54:52 -0500

    💁‍Auth Reducer added

--- src/BudgetingApp.js
+++ src/BudgetingApp.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import BudgetingContext from './context/BudgetingContext';
+import AuthReducer from './reducers/AuthReducer';
 import AppRouter from './routers/AppRouter';
 
 const BudgetingApp = () => {
+  const initAuth = () => {
+    return (
+      JSON.parse(localStorage.getItem('budgeting-auth')) || { logged: false }
+    );
+  };
+
+  const [stateAuth, dispatchAuth] = useReducer(AuthReducer, {}, initAuth);
+
+  useEffect(() => {
+    localStorage.setItem('budgeting-auth', JSON.stringify(stateAuth));
+  }, [stateAuth]);
+
   return (
     <div>
-      <BudgetingContext.Provider value={{ name: 'james' }}>
+      <BudgetingContext.Provider value={{ stateAuth, dispatchAuth }}>
         <AppRouter />
       </BudgetingContext.Provider>
     </div>
