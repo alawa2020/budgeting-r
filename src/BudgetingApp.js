d14a8feb3991d60e102d2d1fbae5246b5988efa9
alawa2020 <huder13168@gmail.com>
2021-12-31 19:45:44 -0500

    🎬clean project and start

---
+++ src/BudgetingApp.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BudgetingApp = () => {
+    return (
+        <div>
+            <h1>Hello to Budgeting App!</h1>
+        </div>
+    )
+}
+
+export default BudgetingApp
