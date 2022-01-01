e68d68b143cd54655623337835d18b7be3dfe87e
alawa2020 <huder13168@gmail.com>
2021-12-31 22:26:33 -0500

    🕵️ private and public routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,14 +1,28 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { useContext } from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import LoginScreen from '../components/login/LoginScreen';
+import BudgetingContext from '../context/BudgetingContext';
 import DashboardRouter from './DashboardRouter';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 const AppRouter = () => {
+  const { stateAuth } = useContext(BudgetingContext);
+
   return (
     <Router>
       <Switch>
-        <Route exact path="/login" component={LoginScreen} />
-        <Route path="/" component={DashboardRouter} />
+        <PublicRoute
+          exact
+          path="/login"
+          component={LoginScreen}
+          isAuthenticated={stateAuth.logged}
+        />
+        <PrivateRoute
+          path="/"
+          component={DashboardRouter}
+          isAuthenticated={stateAuth.logged}
+        />
       </Switch>
     </Router>
   );
---
+++ src/routers/PrivateRoute.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
+
+import PropTypes from 'prop-types';
+
+const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/login" />
+      }
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  isAuthenticated: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+};
+
+export default PrivateRoute;
